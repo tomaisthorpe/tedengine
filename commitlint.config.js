ab8aeb2c3d05d9c68f526f7d003ff91b29f4e90c
Tom Aisthorpe <tomaisthorpe@gmail.com>
2023-07-22 20:18:29 +0100

    chore: add commitlint hook

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
+    'footer-max-length': [0, 'always'],
+    'footer-max-line-length': [0, 'always'],
+    'header-max-length': [0, 'always'],
+    'scope-max-length': [0, 'always'],
+    'subject-max-length': [0, 'always'],
+    'type-max-length': [0, 'always'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'core',
+        'physics',
+        'graphics',
+        '2d',
+        '3d',
+        'components',
+        'cameras',
+        'debug',
+        'ui',
+        'input',
+        'audio',
+        'resources',
+        'gamestate',
+        'release',
+      ],
+    ],
+  },
+};
