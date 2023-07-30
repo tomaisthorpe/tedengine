44cc4a63485ddc929bd1a694090ff9f1f134074f
Tom Aisthorpe <tomaisthorpe@gmail.com>
2023-07-30 16:36:10 +0100

    docs: remove nextra from title

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,6 +1,15 @@
+import { useRouter } from 'next/router';
 export default {
   project: {
     link: 'https://github.com/tomaisthorpe/tedengine',
   },
   logo: <strong>TED</strong>,
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – TED Engine',
+      };
+    }
+  },
 };
