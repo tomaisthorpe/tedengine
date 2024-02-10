f336756a7fc5806ccb65ac00d40920ff614f57ad
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-02-10 00:37:05 +0000

    chore: update package dependencies

---
+++ .changeset/stupid-shirts-arrive.md
@@ -0,0 +1,5 @@
+---
+"@tedengine/ted": patch
+---
+
+Bump dependencies
--- packages/ted/vite.config.ts
+++ packages/ted/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 
   // Uncomment this if you are using workers.
   worker: {
-    plugins: [
+    plugins: () => [
       viteTsConfigPaths({
         root: '../../',
       }),
@@ -34,6 +34,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../dist/packages/ted',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
