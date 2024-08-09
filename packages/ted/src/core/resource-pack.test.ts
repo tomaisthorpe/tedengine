ee9c108e7f5ebff1b8cabe1c5b56a81c95133595
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-08-09 23:23:10 +0100

    test(core): add for resource pack

---
+++ packages/ted/src/core/resource-pack.test.ts
@@ -0,0 +1,68 @@
+import TResourcePack from './resource-pack';
+import TMesh from '../graphics/mesh';
+import TColorMaterial from '../graphics/color-material';
+import TImage from '../graphics/image';
+import TTexture from '../graphics/texture';
+import TTSound from '../audio/sound';
+import TTilemap from '../graphics/tilemap';
+
+describe('TResourcePack', () => {
+  let resourcePack: TResourcePack;
+  let engine: any;
+
+  beforeEach(() => {
+    engine = { resources: { load: jest.fn() } } as any;
+    resourcePack = new TResourcePack(
+      engine,
+      {
+        meshes: ['mesh1'],
+        materials: ['material1'],
+        images: ['image1'],
+        textures: ['texture1'],
+        sounds: ['sound1'],
+        tilemaps: ['tilemap1'],
+      },
+      {
+        meshes: ['mesh2'],
+        materials: ['material2'],
+        images: ['image2'],
+        textures: ['texture2'],
+        sounds: ['sound2'],
+        tilemaps: ['tilemap2'],
+      },
+      { meshes: ['mesh3'] },
+    );
+  });
+
+  test('should merge all configs into one', () => {
+    expect(resourcePack.resources).toEqual({
+      meshes: ['mesh1', 'mesh2', 'mesh3'],
+      materials: ['material1', 'material2'],
+      images: ['image1', 'image2'],
+      textures: ['texture1', 'texture2'],
+      sounds: ['sound1', 'sound2'],
+      tilemaps: ['tilemap1', 'tilemap2'],
+    });
+  });
+
+  test('should load all resources in the resource pack', async () => {
+    const loadMock = jest.spyOn(engine.resources, 'load');
+
+    await resourcePack.load();
+
+    expect(loadMock).toHaveBeenCalledTimes(13);
+    expect(loadMock).toHaveBeenCalledWith(TMesh, 'mesh1');
+    expect(loadMock).toHaveBeenCalledWith(TMesh, 'mesh2');
+    expect(loadMock).toHaveBeenCalledWith(TMesh, 'mesh3');
+    expect(loadMock).toHaveBeenCalledWith(TColorMaterial, 'material1');
+    expect(loadMock).toHaveBeenCalledWith(TColorMaterial, 'material2');
+    expect(loadMock).toHaveBeenCalledWith(TImage, 'image1');
+    expect(loadMock).toHaveBeenCalledWith(TImage, 'image2');
+    expect(loadMock).toHaveBeenCalledWith(TTexture, 'texture1');
+    expect(loadMock).toHaveBeenCalledWith(TTexture, 'texture2');
+    expect(loadMock).toHaveBeenCalledWith(TTSound, 'sound1');
+    expect(loadMock).toHaveBeenCalledWith(TTSound, 'sound2');
+    expect(loadMock).toHaveBeenCalledWith(TTilemap, 'tilemap1');
+    expect(loadMock).toHaveBeenCalledWith(TTilemap, 'tilemap2');
+  });
+});
--- packages/ted/src/core/resource-pack.ts
+++ packages/ted/src/core/resource-pack.ts
@@ -17,7 +17,7 @@ export interface TResourcePackConfig {
 }
 
 export default class TResourcePack {
-  private resources: TResourcePackConfig;
+  public resources: TResourcePackConfig;
 
   constructor(
     private engine: TEngine,
