6ce5f3b64f6816a0037aa79735576df5e9861577
tomaisthorpe <tomaisthorpe@gmail.com>
2024-12-10 22:23:29 +0000

    feat(audio): switch to howler

---
+++ .changeset/smooth-donkeys-drive.md
@@ -0,0 +1,5 @@
+---
+'@tedengine/ted': minor
+---
+
+Use Howler for audio
--- packages/ted/src/audio/audio.ts
+++ packages/ted/src/audio/audio.ts
@@ -1,8 +1,8 @@
 import { v4 as uuidv4 } from 'uuid';
+import { Howl } from 'howler';
 
 export default class TAudio {
-  private ctx?: AudioContext;
-  private sounds: { [key: string]: HTMLAudioElement } = {};
+  private sounds: { [key: string]: Howl } = {};
 
   private _muted = false;
 
@@ -15,35 +15,19 @@ export default class TAudio {
 
     // Loop through all sounds and set the muted property
     Object.values(this.sounds).forEach((sound) => {
-      sound.muted = value;
+      sound.mute(value);
     });
-  }
-
-  /**
-   * Gets the AudioContext. You must do this AFTER the user has interacted with
-   * page. Otherwise Chrome will block the context.
-   *
-   * @returns {AudioContext} ctx
-   */
-  public getContext(): AudioContext {
-    if (!this.ctx) {
-      this.ctx = new AudioContext();
-    }
-
-    return this.ctx;
   }
 
   public async loadSound(url: string): Promise<string> {
     const uuid = uuidv4();
 
-    this.sounds[uuid] = new Audio(url);
+    this.sounds[uuid] = new Howl({ src: [url] });
     return uuid;
   }
 
   public play(uuid: string, volume: number) {
-    if (this.muted) return;
-
-    this.sounds[uuid].volume = volume;
+    this.sounds[uuid].volume(volume);
     this.sounds[uuid].play();
   }
 }
