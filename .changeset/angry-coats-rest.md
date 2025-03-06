2cf3819c27e6e8cda2b79713c2482d0e638f44c1
tomaisthorpe <tomaisthorpe@gmail.com>
2025-03-06 23:42:47 +0000

    chore(cli): create project

---
+++ .changeset/angry-coats-rest.md
@@ -0,0 +1,5 @@
+---
+'@tedengine/cli': minor
+---
+
+Create CLI project
--- commitlint.config.js
+++ commitlint.config.js
@@ -30,6 +30,7 @@ module.exports = {
         'perf',
         'math',
         'editor',
+        'cli',
       ],
     ],
   },
