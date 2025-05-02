0e3bb5d4f4b8a4f406e9a12472a69efff7c54bab
tomaisthorpe <tomaisthorpe@gmail.com>
2025-05-02 18:14:25 +0100

    test: add world ecs tests

--- packages/ted/src/core/world.test.ts
+++ packages/ted/src/core/world.test.ts
@@ -11,6 +11,12 @@ class TestComponent extends TComponent {
   }
 }
 
+class TestComponent2 extends TComponent {
+  constructor(public value: string) {
+    super();
+  }
+}
+
 test('should create a World instance', () => {
   const mockEngine = {} as TEngine;
   const mockGameState = {} as TGameState;
@@ -28,6 +34,25 @@ describe('createEntity', () => {
 
     const entity2 = world.createEntity();
     expect(entity2).toBe(1);
+  });
+});
+
+describe('addComponents', () => {
+  test('should add multiple components to an entity', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const entity = world.createEntity();
+    world.addComponents(entity, [
+      new TestComponent(1),
+      new TestComponent2('test'),
+    ]);
+    expect(world.getComponents(entity)?.has(TestComponent)).toBe(true);
+    expect(world.getComponents(entity)?.get(TestComponent)?.value).toBe(1);
+    expect(world.getComponents(entity)?.has(TestComponent2)).toBe(true);
+    expect(world.getComponents(entity)?.get(TestComponent2)?.value).toBe(
+      'test',
+    );
   });
 });
 
@@ -62,6 +87,36 @@ describe('removeComponent', () => {
     world.addComponent(entity, new TestComponent(1));
     world.removeComponent(entity, TestComponent);
     expect(world.getComponents(entity)?.has(TestComponent)).toBe(false);
+  });
+});
+
+describe('getComponents', () => {
+  test('should return the correct components for an entity', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const entity = world.createEntity();
+    world.addComponents(entity, [
+      new TestComponent(1),
+      new TestComponent2('test'),
+    ]);
+    const components = world.getComponents(entity);
+    expect(components?.has(TestComponent)).toBe(true);
+    expect(components?.get(TestComponent)?.value).toBe(1);
+    expect(components?.has(TestComponent2)).toBe(true);
+    expect(components?.get(TestComponent2)?.value).toBe('test');
+  });
+});
+
+describe('getComponent', () => {
+  test('should return the correct component for an entity', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const entity = world.createEntity();
+    world.addComponent(entity, new TestComponent(1));
+    const component = world.getComponent(entity, TestComponent);
+    expect(component?.value).toBe(1);
   });
 });
 
@@ -114,5 +169,83 @@ describe('addSystem', () => {
     // Verify that all systems were called
     expect(spy1).toHaveBeenCalledBefore(spy2);
     expect(spy2).toHaveBeenCalledBefore(spy3);
+  });
+});
+
+describe('removeSystem', () => {
+  test('should remove a system from the world', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const system1 = new FirstSystem();
+    const system2 = new SecondSystem();
+    const system3 = new ThirdSystem();
+
+    world.addSystem(system1);
+    world.addSystem(system2);
+    world.addSystem(system3);
+
+    world.removeSystem(system2);
+
+    expect(world.systems.length).toBe(2);
+    expect(world.systems).not.toContain(system2);
+  });
+});
+
+describe('queryEntities', () => {
+  test('should return entities that match the given components', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const entity1 = world.createEntity();
+    const entity2 = world.createEntity();
+    const entity3 = world.createEntity();
+
+    world.addComponents(entity1, [new TestComponent(1)]);
+    world.addComponents(entity2, [new TestComponent2('test')]);
+    world.addComponents(entity3, [new TestComponent(3)]);
+
+    const entities = world.queryEntities([TestComponent]);
+    expect(entities).toEqual([entity1, entity3]);
+  });
+
+  test('should return entities that match the given components and exclude the given components', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const entity1 = world.createEntity();
+    const entity2 = world.createEntity();
+    const entity3 = world.createEntity();
+
+    world.addComponents(entity1, [new TestComponent(1)]);
+    world.addComponents(entity2, [
+      new TestComponent(2),
+      new TestComponent2('test'),
+    ]);
+    world.addComponents(entity3, [new TestComponent(3)]);
+
+    const entities = world.queryEntities([TestComponent], [TestComponent2]);
+    expect(entities).toEqual([entity1, entity3]);
+  });
+});
+
+describe('createQuery', () => {
+  test('should create a query with the given components', () => {
+    const mockEngine = {} as TEngine;
+    const mockGameState = {} as TGameState;
+    const world = new TWorld(mockEngine, mockGameState);
+    const query = world.createQuery([TestComponent]);
+    expect(query).toBeDefined();
+
+    const entity1 = world.createEntity();
+    const entity2 = world.createEntity();
+    const entity3 = world.createEntity();
+
+    world.addComponents(entity1, [new TestComponent(1)]);
+    world.addComponents(entity2, [new TestComponent2('test')]);
+    world.addComponents(entity3, [new TestComponent(3)]);
+
+    const entities = query.execute();
+    expect(entities).toEqual([entity1, entity3]);
   });
 });
